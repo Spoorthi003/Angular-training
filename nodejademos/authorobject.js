940b6d5a53641039683cc135878f269cf2540f21
Spoorthi S <Spoorthi.S@Ltts.com>
2020-08-26 22:08:00 +0530

    assignmentsubmitted

---
+++ nodejademos/authorobject.js
@@ -0,0 +1,17 @@
+let author=new Object();
+
+author.id = "001";
+author.Name = "Natasha  Sharma";
+author.Biography = "A well known fiction Writer from delhi ,renowned by mutiple awards";
+author.Birthdate = "09/07/1899";
+author.Email = "natashasharma08@gmail.com";
+author.PhotographUrl = "https://www.getlitt.co/blog/wp-content/uploads/2018/09/5-minutes-with1.jpg";
+author.listofbooks = "Secret to a great life ,Boycott foriegn goods,keep yourself Happy";
+
+console.log(author.id);
+console.log(author.Name);
+console.log(author.Biography);
+console.log(author.Birthdate);
+console.log(author.Email);
+console.log(author.PhotographUrl);
+console.log(author.listofbooks);
