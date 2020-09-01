6c423bf7de4aa97566fecd9387c951c1b369e7b8
Spoorthi S <Spoorthi.S@Ltts.com>
2020-09-01 18:35:09 +0530

    #assignment6folderinbookweb-client02

---
+++ archieve/including Header Footer/src/app/books-web-footer.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-booksweb-footer',
+    styleUrls:[
+        'books-web-footer.component.css'
+    ],
+    templateUrl:'books-web-footer.component.html'
+    
+})
+export class BooksWebFooterComponent{
+}
